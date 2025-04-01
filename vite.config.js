3a4240778cc74c691277e200610410fc1879454f
valter goes <valtergoes@outlook.com>
2025-03-31 21:05:56 -0300

    feat:configurando o sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createSitemap } from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createSitemap({
+      hostname: 'https://maragonesateixeira.adv.br', // Altere para seu domínio real
+      outDir: 'dist',
+      urls: [
+        { url: '/', changefreq: 'daily', priority: 1.0 },
+      ],
   base: '/',
 })
+  ],
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+})
+
