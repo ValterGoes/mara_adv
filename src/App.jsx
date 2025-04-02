f4a44da8ed3af68f2e9bf72b1e31e0b2297c8f8b
valter goes <valtergoes@outlook.com>
2025-04-02 18:40:41 -0300

    feat: ajuste no caminho de indexação

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
 
 import { Router } from "./Router";
 
@@ -8,9 +9,14 @@ import './index.css';
 
 const App = () => {
   return (
+    <>
+      <Helmet>
+        <link rel="canonical" href="https://maragonesateixeira.adv.br/" />
+      </Helmet>
       <BrowserRouter>
         <Router />
       </BrowserRouter>
+    </>
   );
 };
 
