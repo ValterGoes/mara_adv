0d230baaf9b0933560e7aa6d3eb23266f398ac6e
valter goes <valtergoes@outlook.com>
2025-04-01 21:27:06 -0300

    feat: Correção do direcionamento de domínios

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import vitePluginSitemap from 'vite-plugin-sitemap'
+import vitePluginSitemap from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     vitePluginSitemap({
-      hostname: 'https://maragonesateixeira.com.br',
-      outDir: 'dist',
-      sitemapFilename: 'sitemap-com.xml', 
-      urls: [
-        { url: '/', changefreq: 'daily', priority: 1.0 },
-      ],
-    }),
-    vitePluginSitemap({
       hostname: 'https://maragonesateixeira.adv.br',
       outDir: 'dist',
-      sitemapFilename: 'sitemap-adv.xml',
       urls: [
         { url: '/', changefreq: 'daily', priority: 1.0 },
       ],
-    }),
-  ],
   base: '/',
+})
+  ],
   server: {
     port: 3000,
     open: true,
@@ -33,3 +24,4 @@ export default defineConfig({
     sourcemap: true,
   },
 })
+
